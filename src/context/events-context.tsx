c351d1ec56fb41f5d18ec959d9bdd97341fa2223
Ulises <larrabeulises@gmail.com>
2025-07-11 13:37:34 -0300

    Add functions

--- src/context/events-context.tsx
+++ src/context/events-context.tsx
@@ -10,6 +10,18 @@ interface EventsContextType {
   setEvents: (events: Event[]) => void;
   getEventsByMonth: (month: number, year: number) => Promise<void>;
   addEvent: (event: Event) => Promise<void>;
+  getEventsByMonthAndCategory: (
+    month: number,
+    year: number,
+    category: string
+  ) => Promise<Event[]>;
+  getEventsByMonthAndType: (
+    month: number,
+    year: number,
+    type: string
+  ) => Promise<Event[]>;
+  getOutDatedOutcomeEvents: () => Promise<Event[]>;
+  getIncomeOutcomeEvents: () => Promise<Event[]>;
 }
 
 export const EventsContext = createContext<EventsContextType | null>(null);
@@ -45,13 +57,84 @@ export const EventsContextProvider = ({
     setEvents((prevEvents) => [...prevEvents, data[0]]);
   };
 
+  const getEventsByMonthAndCategory = async (
+    month: number,
+    year: number,
+    category: string
+  ) => {
+    const { data: events, error } = await supabase
+      .from("events")
+      .select("*")
+      .eq("month", month)
+      .eq("year", year)
+      .eq("category", category);
+    if (error) {
+      throw new Error("Error al obtener eventos con la categoria " + category);
+    }
+    return events as Event[];
+  };
+
+  const getEventsByMonthAndType = async (
+    month: number,
+    year: number,
+    type: string
+  ) => {
+    const { data: events, error } = await supabase
+      .from("events")
+      .select("*")
+      .eq("month", month)
+      .eq("year", year)
+      .eq("type", type);
+    if (error) {
+      throw new Error("Error al obtener eventos con el tipo " + type);
+    }
+    return events as Event[];
+  };
+
+  const getOutDatedOutcomeEvents = async () => {
+    const { data: events, error } = await supabase
+      .from("events")
+      .select("*")
+      .eq("month", dayjs().month() + 1)
+      .eq("year", dayjs().year())
+      .eq("type", "outcome")
+      .lt("start", dayjs().format("YYYY-MM-DD"));
+    if (error) {
+      throw new Error("Error al obtener eventos");
+    }
+    return events as Event[];
+  };
+
+  const getIncomeOutcomeEvents = async () => {
+    const { data: events, error } = await supabase
+      .from("events")
+      .select("*")
+      .eq("month", dayjs().month() + 1)
+      .eq("year", dayjs().year())
+      .eq("type", "outcome")
+      .gt("start", dayjs().format("YYYY-MM-DD"));
+    if (error) {
+      throw new Error("Error al obtener eventos");
+    }
+    return events as Event[];
+  };
+
   useEffect(() => {
     getEventsByMonth(dayjs().month() + 1, dayjs().year());
   }, []);
 
   return (
     <EventsContext.Provider
-      value={{ events, setEvents, getEventsByMonth, addEvent }}
+      value={{
+        events,
+        setEvents,
+        getEventsByMonth,
+        addEvent,
+        getEventsByMonthAndCategory,
+        getEventsByMonthAndType,
+        getOutDatedOutcomeEvents,
+        getIncomeOutcomeEvents,
+      }}
     >
       {children}
     </EventsContext.Provider>
